fc6bdd1c0795f936394bbffe722fe4ecd01d295e
ehasun15019 <ehasunulislam0202@gmail.com>
2026-07-24 02:59:17 +0600

    create just proxy for authinticate middleware next home page layout

---
+++ proxy.ts
@@ -0,0 +1,31 @@
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const AUTH_ROUTES = ["/login", "/register"];
+// const PUBLIC_ROUTES = ["/"];
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request: NextRequest) {
+    const pathName = request.nextUrl.pathname;
+    const cookieStore = await cookies();
+
+    const token = cookieStore.get("aToken")?.value;
+    const isAuthRoutes = AUTH_ROUTES.includes(pathName)
+
+    if(token && isAuthRoutes) {
+        return NextResponse.redirect(new URL("/", request.url))
+    }
+
+    if(!token && !isAuthRoutes) {
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
+
+    return NextResponse.next()
+}
+ 
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|.*\\.png$).*)'
+  ],
+}
